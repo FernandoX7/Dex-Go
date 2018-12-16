4d2bb61ba5273251ddfb6ce331476e05f9e38306
Fernando Ramirez <framirez1997@gmail.com>
2018-12-16 15:11:38 -0600

    Add function to insert sprite images into the pokemon objects

--- src/app/_services/pokedex.service.ts
+++ src/app/_services/pokedex.service.ts
@@ -4,6 +4,7 @@ import { Observable } from 'rxjs';
 import { map, tap } from 'rxjs/operators';
 import { CacheService } from 'ionic-cache';
 import { DexGoConstants } from '../_utilities/DexGoConstants';
+import { map as _map } from 'lodash';
 
 @Injectable({
   providedIn: 'root'
@@ -21,13 +22,13 @@ export class PokedexService {
     const cacheKey = DexGoConstants.CACHE_ALL_POKEMON;
     let request = this.http.get(url)
       .pipe(
-        map((data: any) => data.results),
-        tap((pokemon) => this.pokemon = pokemon));
+        map((data: any) => this.insertSpriteImages(data.results)),
+        tap((pokemon: any) => this.pokemon = pokemon));
 
     if (refresher) {
       request = this.http.get(url)
         .pipe(
-          map((data: any) => data.results),
+          map((data: any) => this.insertSpriteImages(data.results)),
           tap((pokemon) => this.pokemon = pokemon));
       const delayType = 'all'; // Send a request to the server every time
       const ttl = 60 * 60 * 12; // 12 hours
@@ -48,6 +49,24 @@ export class PokedexService {
 
   getPokemonById(id: number) {
     return this.http.get(`https://pokeapi.co/api/v2/pokemon/${id}/`);
+  }
+
+  insertSpriteImages(pokemon) {
+    return _map(pokemon, (item: any) => {
+      // Get the ID of the pokemon from its url property
+      const id = item.url.split('/')[6];
+      item.sprites = {
+        back_default: `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/back/${id}.png`,
+        back_female: null,
+        back_shiny: `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/back/shiny/${id}.png`,
+        back_shiny_female: null,
+        front_default: `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${id}.png`,
+        front_female: null,
+        front_shiny: `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/shiny/${id}.png`,
+        front_shiny_female: null
+      };
+      return item;
+    });
   }
 
 }
