3592a276ac350d000e12bc3eacd34f7b42cd48b4
Fernando Ramirez <framirez1997@gmail.com>
2018-12-16 20:59:13 -0600

    Retrieve pokemon id on navigation landing

--- src/app/_pages/pokemon-detail/pokemon-detail.page.ts
+++ src/app/_pages/pokemon-detail/pokemon-detail.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-pokemon-detail',
@@ -7,10 +8,13 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PokemonDetailPage implements OnInit {
 
-  constructor() {
+  pokemonID: any;
+
+  constructor(private route: ActivatedRoute) {
   }
 
   ngOnInit() {
+    this.pokemonID = this.route.snapshot.paramMap.get('id');
   }
 
 }
