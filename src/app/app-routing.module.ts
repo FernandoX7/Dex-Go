364e1e78932a7a9a4afc1bb5271f1d20f18fc4b0
Fernando Ramirez <framirez1997@gmail.com>
2018-12-16 20:44:04 -0600

    Add base for pokemon detail page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -14,6 +14,10 @@ const routes: Routes = [
   {
     path: 'list',
     loadChildren: './list/list.module#ListPageModule'
+  },
+  {
+    path: 'pokemon-detail',
+    loadChildren: './_pages/pokemon-detail/pokemon-detail.module#PokemonDetailPageModule'
   }
 ];
 
